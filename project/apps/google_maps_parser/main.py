70fb83117912ac160156047f8377e7dbbb6bc9fe
Madi-S <shaiken.m@mail.ru>
2023-12-31 19:36:53 +0600

    Link extractor ready

--- project/apps/google_maps_parser/main.py
+++ project/apps/google_maps_parser/main.py
@@ -1,60 +1,86 @@
 import asyncio
-from playwright.async_api import async_playwright, Playwright, Browser, Page
-from time import sleep
+from playwright.async_api import async_playwright, Playwright, Browser, Page, ElementHandle
+from time import sleep, time
 
 
-query = 'ресторан'
+query = 'спортазл'
 browser_params = {'headless': False, 'proxy': None, 'slow_mo': 150}
-page_params = {'java_script_enabled':True, 'bypass_csp': True, 'locale':'en-GB'}
-coords = (51.0968431,71.428286) # so that location is defined by the user, or get coords by input query city
+page_params = {'java_script_enabled': True,
+               'bypass_csp': True, 'locale': 'en-GB'}
+# so that location is defined by the user, or get coords by input query city
+coords = (51.0968431, 71.428286)
 coords = list(map(str, coords))
 url = f'https://www.google.com/maps/@{''.join(coords)},13z?entry=ttu'
 
+
 async def run(playwright: Playwright):
     chromium = playwright.chromium
-    
+
     browser: Browser = await chromium.launch(**browser_params)
     page: Page = await browser.new_page(**page_params)
-    
+
     await page.goto('https://www.google.com/maps')
     await asyncio.sleep(3)
-    
+
     # find by query
     # go over every search result
     # parse its telephone number and other information
     # once there are no more results, save data and exit
-    
+
     await page.click('#searchboxinput')
     await page.keyboard.type(query, delay=20)
     await page.keyboard.press('Enter')
-    await page.wait_for_selector('.hfpxzc') # wait for results to appear
-    
-    end_text = 'You\'ve reached the end of the list.'
+    await page.wait_for_selector('.hfpxzc')  # wait for results to appear
+
     end_locator = await page.query_selector('.m6QErb.tLjsW.eKbjU')
-    while not end_locator:
-        # try to scroll down
-        ...
+
+    leftbar = await page.query_selector('[role="main"]')
+    await leftbar.hover()
+    sleep(5)
+    print('Hovered on leftbar, ready to scroll')
+
+    start_scroll_time = time()
+    scroll_time_duration_s = 50
+
+    while True:
+        await page.mouse.wheel(0, 1000)
+        sleep(0.2)
+        print('Scroll down')
+
         end_locator = await page.query_selector('.m6QErb.tLjsW.eKbjU')
+        print('End locator:', end_locator)
+        
+        finish_scroll_time = time()
     
-    result_links = ...
-    
-    # i = 0
-    # scroll_i = 0
-    # while True:
-    #     results = await page.query_selector_all('.hfpxzc')
-    #     print(results)
-    #     result = results[i]
-    #     await result.click()
-    #     print('visible', await result.is_visible())
-    #     print('element', result.as_element())
-    #     print('props', await result.get_properties())
-    #     break
-    
-    
+        if end_locator is not None:
+            print('End locator found, breaking')
+            break
+        
+        if finish_scroll_time - start_scroll_time > scroll_time_duration_s:
+            print(f'Timeout exceed {scroll_time_duration_s} seconds. Breaking automatically')
+            break
+        
+    link_elements = await page.query_selector_all('a.hfpxzc')
+    print('Link elements:', link_elements)
+
+    for link_element in link_elements:
+        link = await link_element.get_attribute('href')
+        print(link)
+
+    print('Finished')
     sleep(999999)
     await browser.close()
+
+# enter query
+# scroll all the way down
+# extract all hrefs of results
+# go over each href and extract information
+# save and format the info
+
 
 async def main():
     async with async_playwright() as playwright:
         await run(playwright)
+
+
 asyncio.run(main())
